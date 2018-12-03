00cb4ad61dfb86916c000a50053ee9fd52e14fa6
yamert89 <softoad@yandex.ru>
2018-12-03 21:25:53 +0300

    coinmarketcap strategy started

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 CryptoWidget#2
 published on https://play.google.com/store/apps/details?id=net.ucoz.softoad.cryptowidget
+
+https://pro.coinmarketcap.com/api/v1#section/Quick-Start-Guide
--- app/src/main/java/net/ucoz/softoad/cryptowidget_2/strategies/CoinMarketCapStrategy.java
+++ app/src/main/java/net/ucoz/softoad/cryptowidget_2/strategies/CoinMarketCapStrategy.java
@@ -2,7 +2,14 @@ package net.ucoz.softoad.cryptowidget_2.strategies;
 
 import com.google.gson.JsonElement;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
 public class CoinMarketCapStrategy extends Strategy {
+
+    private String apiKey = "9453bb8b-d186-4d67-99ea-bcd4de0cbf32";
 
     public CoinMarketCapStrategy(String name, String cur1, String cur2) {
         super(name, cur1, cur2);
@@ -10,7 +17,36 @@ public class CoinMarketCapStrategy extends Strategy {
 
     @Override
     public Object[] connection() {
-        return null;
+        System.out.println("name = [" + name + "], cur1 = [" + cur1 + "], cur2 = [" + cur2 + "]");
+        Connection.Response response = null;
+        String url = "https://api.coingecko.com/api/v3/coins/" + name + "?localization=ru";
+        try {
+
+            System.out.println("URL : " + url);
+            response = Jsoup.connect(url)
+                    .header(":authority","api.coingecko.com")
+                    .header("accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+                    //.header("accept-encoding","zip, deflate, br")
+                    //.header("accept-language","ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7")
+                    .header("cache-control","max-age=0")
+                    .header("upgrade-insecure-requests","1")
+                    .referrer("https://api.coingecko.com")
+                    .timeout(15000)
+                    .ignoreContentType(true)
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36 OPR/56.0.3051.99")
+                    .execute();
+        }catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Повторное подключение");
+            try {
+                response = Jsoup.connect(url).ignoreContentType(true).execute();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+                return new Object[]{null, response.statusCode()};
+            }
+        }
+
+        return new Object[]{response, response.statusCode()};
     }
 
     @Override
